a4d0ebad9fdfd888a72009dcbe5d5ac89acd5af6
dwandrew <danwandrew@gmail.com>
2020-09-04 13:43:26 +0100

    added alphabet filter to animals lists

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,5 @@
 const BASEURL= "http://localhost:3000";
+const ALPHABETARRAY = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 
 document.addEventListener("DOMContentLoaded", populateSelectOptions)
 
@@ -7,11 +8,13 @@ let torso = () => document.querySelector('select#chimera-torso');
 let legs = () => document.querySelector('select#chimera-legs');
 let wings = () => document.querySelector('select#chimera-wings');
 let tail = () => document.querySelector('select#chimera-tail');
+let letterFilters = () => document.getElementsByClassName("letter-filter")
 
 function populateSelectOptions(){
 fetch(BASEURL + '/animals')
 .then(resp => resp.json())
 .then(animals => {
+
 addOptionsTo(head(), animals)
 addOptionsTo(torso(), animals)
 addWingOptionsTo(wings(), animals)
@@ -64,3 +67,15 @@ function addLegOptionsTo(list, listToAdd){
         list.add(option)}
     });
     }
+
+function populateLetterFilters(list, letters){
+    for (let i=0; i<list.length; i++)
+    {letters.forEach(element =>{
+        let option = document.createElement("option")
+        option.innerText = element
+        option.value = element
+        list[i].add(option)
+         }
+        )}
+        
+}
