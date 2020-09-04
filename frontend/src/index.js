0d006c51ded4ebfec72763805b00aca737c23bdf
dwandrew <danwandrew@gmail.com>
2020-09-04 16:26:56 +0100

    auto updating lists of names

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,10 @@
 const BASEURL= "http://localhost:3000";
 const ALPHABETARRAY = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 
-document.addEventListener("DOMContentLoaded", populateSelectOptions)
+document.addEventListener("DOMContentLoaded", (event) => {
+    populateLetterFilters(letterFilters(), ALPHABETARRAY);
+}
+    )
 
 let head = () => document.querySelector('select#chimera-head');
 let torso = () => document.querySelector('select#chimera-torso');
@@ -10,72 +13,131 @@ let wings = () => document.querySelector('select#chimera-wings');
 let tail = () => document.querySelector('select#chimera-tail');
 let letterFilters = () => document.getElementsByClassName("letter-filter")
 
-function populateSelectOptions(){
+function populateSelectOptions(list){
+ 
 fetch(BASEURL + '/animals')
 .then(resp => resp.json())
 .then(animals => {
-
-addOptionsTo(head(), animals)
-addOptionsTo(torso(), animals)
-addWingOptionsTo(wings(), animals)
-addLegOptionsTo(legs(), animals)
-addTailOptionsTo(tail(), animals)
+ 
+    switch (list.id){
+        case "chimera-head":
+            return addOptionsTo(list, animals)
+        break;
+        case "chimera-torso":
+        return addOptionsTo(list, animals)
+        break;
+        case "chimera-wings":
+            return addWingOptionsTo(list, animals)
+        break;
+            
+        case "chimera-legs":
+            return addLegOptionsTo(list, animals)
+        break;
+                
+            case "chimera-tail":
+            return addTailOptionsTo(list, animals)
+        break;
+    }
 })
 }
+
+
 function addOptionsTo(list, listToAdd){
+   let filterLetter = list.parentNode.querySelector(".letter-filter").value
 listToAdd.forEach(element => {
+    if (filterLetter === element.name[0]){
     let option = document.createElement("option")
     option.appendChild( document.createTextNode(element.name))
     option.id= `${list}-${element.id}`
     option.value = element.name;
     option.className = "Add-Animal";
     list.add(option)
+    }
 });
 }
 
 function addWingOptionsTo(list, listToAdd){
+
+    let filterLetter = list.parentNode.querySelector(".letter-filter").value
 listToAdd.forEach(element => {
     if(element.wings=== true){
+        if (filterLetter === element.name[0]){
     let option = document.createElement("option")
     option.appendChild( document.createTextNode(element.name))
     option.id= `${list}-${element.id}`
     option.value = element.name;
     option.className = "Add-Animal";
-    list.add(option)}
+    list.add(option)}}
 });
 }
 
 function addTailOptionsTo(list, listToAdd){
+    let filterLetter = list.parentNode.querySelector(".letter-filter").value
     listToAdd.forEach(element => {
         if(element.tail=== true){
+            if (filterLetter === element.name[0]){
         let option = document.createElement("option")
         option.appendChild( document.createTextNode(element.name))
         option.id= `${list}-${element.id}`
         option.value = element.name;
         option.className = "Add-Animal";
-        list.add(option)}
+        list.add(option)}}
     });
     }
 function addLegOptionsTo(list, listToAdd){
+
+    let filterLetter = list.parentNode.querySelector(".letter-filter").value
     listToAdd.forEach(element => {
         if(element.legs=== true){
+            if (filterLetter === element.name[0]){
         let option = document.createElement("option")
         option.appendChild( document.createTextNode(element.name))
         option.id= `${list}-${element.id}`
         option.value = element.name;
         option.className = "Add-Animal";
-        list.add(option)}
+        list.add(option)}}
     });
     }
 
 function populateLetterFilters(list, letters){
     for (let i=0; i<list.length; i++)
-    {letters.forEach(element =>{
+    {
+        
+        letters.forEach(element =>{
         let option = document.createElement("option")
         option.innerText = element
         option.value = element
         list[i].add(option)
+        } )
+        list[i].addEventListener('change', (event) =>{ 
+            let letter = event.target.value
+            let field =  event.target.parentNode.children[3]
+            field.innerHTML = ""
+            let repopFilter = field.id.split("-")[1]
+            if (repopFilter === "head"){
+                populateSelectOptions(head())
+            }
+            else if (repopFilter === "torso"){
+                populateSelectOptions(torso())
+            }
+            else if (repopFilter === "wings"){
+                populateSelectOptions(wings())
+            }
+            else if (repopFilter === "legs"){
+                populateSelectOptions(legs())
+            }
+            else if (repopFilter === "tail"){
+                populateSelectOptions(tail())
+            }
          }
         )}
         
 }
+
+function addNoneOption(list){
+    let noneOption = document.createElement("option")
+    noneOption.innerText = "None"
+    noneOption.value = "none"
+    list.add(noneOption)
+}
+
